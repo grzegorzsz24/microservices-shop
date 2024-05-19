9628fdabda7e4e1cb5f5d91798a16e695190429c
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-19 18:25:07 +0200

    Create integration test for adding a product

--- product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.example.productservice.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Builder
 public class ProductRequest {
     private String name;
     private String description;

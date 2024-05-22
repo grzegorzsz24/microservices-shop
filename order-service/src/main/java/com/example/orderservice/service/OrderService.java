4db160e337ba8a586e76fe19ba5bf308b188c456
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 15:24:27 +0200

    Add transactional annotation over whole class

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -8,12 +8,14 @@ import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     private final OrderLineItemsMapper orderLineItemsMapper;
     private final OrderRepository orderRepository;

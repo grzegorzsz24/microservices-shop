360dbfc8f006d5808b101e49f53c1aef875cfee1
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 14:56:28 +0200

    Create logic for saving an order

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.dto.OrderResponse;
+import com.example.orderservice.mapper.OrderLineItemsMapper;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderLineItemsMapper orderLineItemsMapper;
+    private final OrderRepository orderRepository;
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsListDto()
+                .stream()
+                .map(orderLineItemsMapper::mapFromDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setOrderLineItemsListDto(order.getOrderLineItemsList()
+                .stream()
+                .map(orderLineItemsMapper::mapToDto)
+                .toList());
+
+        return orderResponse;
+    }
+}

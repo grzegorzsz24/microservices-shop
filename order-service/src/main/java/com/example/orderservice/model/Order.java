1095a0ff9d8c4b2bf5ecfbddf723f1d26c4cf4a9
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 14:55:19 +0200

    Change order table name

--- order-service/src/main/java/com/example/orderservice/model/Order.java
+++ order-service/src/main/java/com/example/orderservice/model/Order.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)

895919ccfac9a1776bfc57df0269f3e2aecb6ab4
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 15:28:09 +0200

    Create bean for loading sample data

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,33 @@
 package com.example.inventoryservice;
 
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("Iphone_13");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("Iphone_15");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.saveAll(List.of(inventory1, inventory));
+        };
+    }
 }

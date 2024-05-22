c7b9bb798ff542ba0d10d31ca9c5a742c1bbc652
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 14:57:06 +0200

    Add getter and setter annotations to order line items dto

--- order-service/src/main/java/com/example/orderservice/dto/OrderLineItemsDto.java
+++ order-service/src/main/java/com/example/orderservice/dto/OrderLineItemsDto.java
@@ -1,7 +1,12 @@
 package com.example.orderservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class OrderLineItemsDto {
     private Long id;
     private String skuCode;

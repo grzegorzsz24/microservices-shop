0040f6cae51b5d683a25768d51a58afee6193c7a
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-22 15:11:47 +0200

    Create inventory repository and inventory model

---
+++ inventory-service/src/main/java/com/example/inventoryservice/model/Inventory.java
@@ -0,0 +1,19 @@
+package com.example.inventoryservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
